cc5bf8379ec6edd249922b408da3be5b347b1fc8
zhanggong <zhanggong@58.com>
2019-07-16 13:44:52 +0800

    transaction-事务模块已阅

--- src/main/java/org/apache/ibatis/transaction/Transaction.java
+++ src/main/java/org/apache/ibatis/transaction/Transaction.java
@@ -1,17 +1,17 @@
 /**
- *    Copyright 2009-2019 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2019 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.transaction;
 
@@ -19,6 +19,8 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 /**
+ * 事务接口
+ * 我们一般不用transaction 包里的事务，而是用mybatis-spring 中的 SpringManagedTransaction.java 当然也是实现这个接口
  * Wraps a database connection.
  * Handles the connection lifecycle that comprises: its creation, preparation, commit/rollback and close.
  *
@@ -26,35 +28,45 @@ import java.sql.SQLException;
  */
 public interface Transaction {
 
-  /**
-   * Retrieve inner database connection.
-   * @return DataBase connection
-   * @throws SQLException
-   */
-  Connection getConnection() throws SQLException;
+    /**
+     * 获取数据库连接
+     * Retrieve inner database connection.
+     *
+     * @return DataBase connection
+     * @throws SQLException
+     */
+    Connection getConnection() throws SQLException;
 
-  /**
-   * Commit inner database connection.
-   * @throws SQLException
-   */
-  void commit() throws SQLException;
+    /**
+     * 提交数据库事务
+     * Commit inner database connection.
+     *
+     * @throws SQLException
+     */
+    void commit() throws SQLException;
 
-  /**
-   * Rollback inner database connection.
-   * @throws SQLException
-   */
-  void rollback() throws SQLException;
+    /**
+     * 回滚
+     * Rollback inner database connection.
+     *
+     * @throws SQLException
+     */
+    void rollback() throws SQLException;
 
-  /**
-   * Close inner database connection.
-   * @throws SQLException
-   */
-  void close() throws SQLException;
+    /**
+     * 关闭数据库连接
+     * Close inner database connection.
+     *
+     * @throws SQLException
+     */
+    void close() throws SQLException;
 
-  /**
-   * Get transaction timeout if set.
-   * @throws SQLException
-   */
-  Integer getTimeout() throws SQLException;
+    /**
+     * 获取事务超时时间，如果设置了的话
+     * Get transaction timeout if set.
+     *
+     * @throws SQLException
+     */
+    Integer getTimeout() throws SQLException;
 
 }
--- src/main/java/org/apache/ibatis/transaction/jdbc/JdbcTransaction.java
+++ src/main/java/org/apache/ibatis/transaction/jdbc/JdbcTransaction.java
@@ -1,23 +1,19 @@
 /**
- *    Copyright 2009-2019 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2019 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.transaction.jdbc;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-import javax.sql.DataSource;
 
 import org.apache.ibatis.logging.Log;
 import org.apache.ibatis.logging.LogFactory;
@@ -25,126 +21,198 @@ import org.apache.ibatis.session.TransactionIsolationLevel;
 import org.apache.ibatis.transaction.Transaction;
 import org.apache.ibatis.transaction.TransactionException;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 /**
  * {@link Transaction} that makes use of the JDBC commit and rollback facilities directly.
  * It relies on the connection retrieved from the dataSource to manage the scope of the transaction.
  * Delays connection retrieval until getConnection() is called.
  * Ignores commit or rollback requests when autocommit is on.
+ * <p>
+ * 实现 Transaction 接口，基于 JDBC 的事务实现类
  *
  * @author Clinton Begin
- *
  * @see JdbcTransactionFactory
  */
 public class JdbcTransaction implements Transaction {
 
-  private static final Log log = LogFactory.getLog(JdbcTransaction.class);
+    private static final Log log = LogFactory.getLog(JdbcTransaction.class);
 
-  protected Connection connection;
-  protected DataSource dataSource;
-  protected TransactionIsolationLevel level;
-  protected boolean autoCommit;
+    /**
+     * 数据库连接
+     */
+    protected Connection connection;
+    /**
+     * 数据源
+     */
+    protected DataSource dataSource;
+    /**
+     * 事务隔离级别
+     */
+    protected TransactionIsolationLevel level;
+    /**
+     * 是否自动提交
+     */
+    protected boolean autoCommit;
 
-  public JdbcTransaction(DataSource ds, TransactionIsolationLevel desiredLevel, boolean desiredAutoCommit) {
-    dataSource = ds;
-    level = desiredLevel;
-    autoCommit = desiredAutoCommit;
-  }
-
-  public JdbcTransaction(Connection connection) {
-    this.connection = connection;
-  }
-
-  @Override
-  public Connection getConnection() throws SQLException {
-    if (connection == null) {
-      openConnection();
+    /**
+     * 构造方法
+     *
+     * @param ds
+     * @param desiredLevel
+     * @param desiredAutoCommit
+     */
+    public JdbcTransaction(DataSource ds, TransactionIsolationLevel desiredLevel, boolean desiredAutoCommit) {
+        dataSource = ds;
+        level = desiredLevel;
+        autoCommit = desiredAutoCommit;
     }
-    return connection;
-  }
 
-  @Override
-  public void commit() throws SQLException {
-    if (connection != null && !connection.getAutoCommit()) {
-      if (log.isDebugEnabled()) {
-        log.debug("Committing JDBC Connection [" + connection + "]");
-      }
-      connection.commit();
+    /**
+     * 构造方法
+     *
+     * @param connection
+     */
+    public JdbcTransaction(Connection connection) {
+        this.connection = connection;
     }
-  }
 
-  @Override
-  public void rollback() throws SQLException {
-    if (connection != null && !connection.getAutoCommit()) {
-      if (log.isDebugEnabled()) {
-        log.debug("Rolling back JDBC Connection [" + connection + "]");
-      }
-      connection.rollback();
-    }
-  }
-
-  @Override
-  public void close() throws SQLException {
-    if (connection != null) {
-      resetAutoCommit();
-      if (log.isDebugEnabled()) {
-        log.debug("Closing JDBC Connection [" + connection + "]");
-      }
-      connection.close();
-    }
-  }
-
-  protected void setDesiredAutoCommit(boolean desiredAutoCommit) {
-    try {
-      if (connection.getAutoCommit() != desiredAutoCommit) {
-        if (log.isDebugEnabled()) {
-          log.debug("Setting autocommit to " + desiredAutoCommit + " on JDBC Connection [" + connection + "]");
+    /**
+     * 获取连接
+     *
+     * @return
+     * @throws SQLException
+     */
+    @Override
+    public Connection getConnection() throws SQLException {
+        if (connection == null) {
+            // 如果连接为空则打开连接
+            openConnection();
         }
-        connection.setAutoCommit(desiredAutoCommit);
-      }
-    } catch (SQLException e) {
-      // Only a very poorly implemented driver would fail here,
-      // and there's not much we can do about that.
-      throw new TransactionException("Error configuring AutoCommit.  "
-          + "Your driver may not support getAutoCommit() or setAutoCommit(). "
-          + "Requested setting: " + desiredAutoCommit + ".  Cause: " + e, e);
+        return connection;
     }
-  }
 
-  protected void resetAutoCommit() {
-    try {
-      if (!connection.getAutoCommit()) {
-        // MyBatis does not call commit/rollback on a connection if just selects were performed.
-        // Some databases start transactions with select statements
-        // and they mandate a commit/rollback before closing the connection.
-        // A workaround is setting the autocommit to true before closing the connection.
-        // Sybase throws an exception here.
+    /**
+     * 打开数据库连接，其实就是从数据链接池中获取一个连接
+     * 并且设置相关的参数
+     *
+     * @throws SQLException
+     */
+    protected void openConnection() throws SQLException {
         if (log.isDebugEnabled()) {
-          log.debug("Resetting autocommit to true on JDBC Connection [" + connection + "]");
+            log.debug("Opening JDBC Connection");
         }
-        connection.setAutoCommit(true);
-      }
-    } catch (SQLException e) {
-      if (log.isDebugEnabled()) {
-        log.debug("Error resetting autocommit to true "
-            + "before closing the connection.  Cause: " + e);
-      }
+        connection = dataSource.getConnection();
+        if (level != null) {
+            connection.setTransactionIsolation(level.getLevel());
+        }
+        // 设置预期是否自动提交
+        setDesiredAutoCommit(autoCommit);
     }
-  }
 
-  protected void openConnection() throws SQLException {
-    if (log.isDebugEnabled()) {
-      log.debug("Opening JDBC Connection");
+    /**
+     * 设置预期是否自动提交
+     *
+     * @param desiredAutoCommit
+     */
+    protected void setDesiredAutoCommit(boolean desiredAutoCommit) {
+        try {
+            if (connection.getAutoCommit() != desiredAutoCommit) {
+                if (log.isDebugEnabled()) {
+                    log.debug("Setting autocommit to " + desiredAutoCommit + " on JDBC Connection [" + connection + "]");
+                }
+                connection.setAutoCommit(desiredAutoCommit);
+            }
+        } catch (SQLException e) {
+            // Only a very poorly implemented driver would fail here,
+            // and there's not much we can do about that.
+            // 翻译一下
+            // 只有很垃圾的数据库驱动实现才会在这里出现错误
+            // 而且，遇见这样的我们也没得办法啊。
+            throw new TransactionException("Error configuring AutoCommit.  "
+                    + "Your driver may not support getAutoCommit() or setAutoCommit(). "
+                    + "Requested setting: " + desiredAutoCommit + ".  Cause: " + e, e);
+        }
     }
-    connection = dataSource.getConnection();
-    if (level != null) {
-      connection.setTransactionIsolation(level.getLevel());
+
+    /**
+     * 提交事务
+     * connection != null && !connection.getAutoCommit()
+     * connection 不能为空，且非自动提交事务
+     *
+     * @throws SQLException
+     */
+    @Override
+    public void commit() throws SQLException {
+        if (connection != null && !connection.getAutoCommit()) {
+            if (log.isDebugEnabled()) {
+                log.debug("Committing JDBC Connection [" + connection + "]");
+            }
+            connection.commit();
+        }
     }
-    setDesiredAutoCommit(autoCommit);
-  }
 
-  @Override
-  public Integer getTimeout() throws SQLException {
-    return null;
-  }
+    /**
+     * 回滚事务
+     * connection 不能为空，且非自动提交事务
+     *
+     * @throws SQLException
+     */
+    @Override
+    public void rollback() throws SQLException {
+        if (connection != null && !connection.getAutoCommit()) {
+            if (log.isDebugEnabled()) {
+                log.debug("Rolling back JDBC Connection [" + connection + "]");
+            }
+            connection.rollback();
+        }
+    }
 
+    /**
+     * 关闭连接
+     *
+     * @throws SQLException
+     */
+    @Override
+    public void close() throws SQLException {
+        if (connection != null) {
+            resetAutoCommit();
+            if (log.isDebugEnabled()) {
+                log.debug("Closing JDBC Connection [" + connection + "]");
+            }
+            connection.close();
+        }
+    }
+
+    /**
+     * 重置事务，设置事务为自动提交
+     */
+    protected void resetAutoCommit() {
+        try {
+            if (!connection.getAutoCommit()) {
+                // MyBatis does not call commit/rollback on a connection if just selects were performed.
+                // Some databases start transactions with select statements
+                // and they mandate a commit/rollback before closing the connection.
+                // A workaround is setting the autocommit to true before closing the connection.
+                // Sybase throws an exception here.
+                if (log.isDebugEnabled()) {
+                    log.debug("Resetting autocommit to true on JDBC Connection [" + connection + "]");
+                }
+                connection.setAutoCommit(true);
+            }
+        } catch (SQLException e) {
+            if (log.isDebugEnabled()) {
+                log.debug("Error resetting autocommit to true "
+                        + "before closing the connection.  Cause: " + e);
+            }
+        }
+    }
+
+
+    @Override
+    public Integer getTimeout() throws SQLException {
+        return null;
+    }
 }
