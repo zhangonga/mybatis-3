d677c0e5711431f300eaaa6e5608b0195c945b97
zhanggong <zhanggong@58.com>
2019-07-18 17:55:28 +0800

    annotations-注解模块已阅

--- src/main/java/org/apache/ibatis/annotations/Result.java
+++ src/main/java/org/apache/ibatis/annotations/Result.java
@@ -1,49 +1,91 @@
 /**
- *    Copyright 2009-2016 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2016 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.annotations;
+
+import org.apache.ibatis.type.JdbcType;
+import org.apache.ibatis.type.TypeHandler;
+import org.apache.ibatis.type.UnknownTypeHandler;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import org.apache.ibatis.type.JdbcType;
-import org.apache.ibatis.type.TypeHandler;
-import org.apache.ibatis.type.UnknownTypeHandler;
-
 /**
+ * 结果字段的注解
+ *
  * @author Clinton Begin
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({})
 public @interface Result {
-  boolean id() default false;
+    /**
+     * 是否是id
+     *
+     * @return
+     */
+    boolean id() default false;
 
-  String column() default "";
+    /**
+     * 数据库列名
+     *
+     * @return
+     */
+    String column() default "";
 
-  String property() default "";
+    /**
+     * java entity 的属性
+     *
+     * @return
+     */
+    String property() default "";
 
-  Class<?> javaType() default void.class;
+    /**
+     * Java entity 的属性的类型
+     *
+     * @return
+     */
+    Class<?> javaType() default void.class;
 
-  JdbcType jdbcType() default JdbcType.UNDEFINED;
+    /**
+     * 数据库字段的类型
+     *
+     * @return
+     */
+    JdbcType jdbcType() default JdbcType.UNDEFINED;
 
-  Class<? extends TypeHandler> typeHandler() default UnknownTypeHandler.class;
+    /**
+     * 指定类型转换器，默认是 UnknownTypeHandler , 不过 UnknownTypeHandler 会根据数据类型转换成功具体类型的转换器
+     *
+     * @return
+     */
+    Class<? extends TypeHandler> typeHandler() default UnknownTypeHandler.class;
 
-  One one() default @One;
+    /**
+     * 多对一的注解
+     *
+     * @return
+     */
+    One one() default @One;
 
-  Many many() default @Many;
+    /**
+     * 一对多的注解
+     *
+     * @return
+     */
+    Many many() default @Many;
 }
--- src/main/java/org/apache/ibatis/annotations/package-info.java
+++ src/main/java/org/apache/ibatis/annotations/package-info.java
@@ -1,20 +1,24 @@
 /**
- *    Copyright 2009-2015 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
-/**
+ * Copyright 2009-2015 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * <p>
  * Contains all the annotation that are used in mapper interfaces
+ * <p>
+ * mybatis  Mapper 接口上编写简单的数据库 SQL 操作代码的所有注解
+ * 增删改查： @Insert、@Update、@Delete、@Select、@MapKey、@Options、@SelelctKey、@Param、@InsertProvider、@UpdateProvider、@DeleteProvider、@SelectProvider
+ * 结果集映射： @Results、@Result、@ResultMap、@ResultType、@ConstructorArgs、@Arg、@One、@Many、@TypeDiscriminator、@Case
+ * 缓存： @CacheNamespace、@Property、@CacheNamespaceRef、@Flush
  */
 package org.apache.ibatis.annotations;
 
